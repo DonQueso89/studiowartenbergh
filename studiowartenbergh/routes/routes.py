5477e9eb4a27e119bdcd1409cde63afc40cb4acc
Kees van Ekeren <kg.v.ekeren@gmail.com>
2018-09-02 12:28:52 +0200

    Added media content. Added routes for contact and about. Restyled stuff a bit.

--- studiowartenbergh/routes/routes.py
+++ studiowartenbergh/routes/routes.py
@@ -2,9 +2,19 @@ from flask import render_template
 
 
 def index():
-    return render_template('base.html')
+    return render_template('home.html')
+
+
+def contact():
+    return render_template('contact.html')
+
+
+def about():
+    return render_template('about.html')
 
 
 routes = (
     (index, '/'),
+    (contact, '/contact'),
+    (about, '/about'),
 )
