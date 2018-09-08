458e5378d86f41b836e014d99ed24d3f71b38d60
Kees van Ekeren <kg.v.ekeren@gmail.com>
2018-09-08 13:55:59 +0200

    Code + config for running in uWSGI, fix js transitioning from and to modal. Some more styling of homepage

--- studiowartenbergh/static/manage_homepage.js
+++ studiowartenbergh/static/manage_homepage.js
@@ -1,21 +1,33 @@
 window.addEventListener('load', function() {
+
     var modal = document.getElementsByClassName('modal')[0]
+    // Transitioning to the modal
+    var items = Array.from(document.getElementsByClassName('item-container'));
+    items.forEach(function(container) {
+        container.lastElementChild.onclick = function(e) {
+            var itemToDisplay = modal.appendChild(container.firstElementChild)
+            modal.style.display = 'block' // show modal
+            itemToDisplay.style.display = 'inline-block' // show item
+        }
+    })
+
+
+    // Transitioning back from the modal
     window.onclick = function(e) {
-        if (e.target == modal) {
+        /*
+         * If we're showing the modal: 
+        * Lookup the item's corresponding container by id.
+        * Insert the full item back into the item container as the first
+        * child.
+        */ 
+        if (e.target == modal && e.target.style.display == 'block') {
+            var elemToMove = modal.removeChild(modal.firstElementChild)
+            var parent = document.getElementById(elemToMove.id)
+            parent.insertBefore(elemToMove, parent.firstElementChild)
+            elemToMove.style.display = 'none';
             modal.style.display = 'none';
         }
     }
 
-    var items = document.getElementsByClassName('item-abstract');
-
-    for (var i = 0; i < items.length; i++) {
-        var item = items[i];
-        item.onclick = function() {
-            var fullItem = item.parentElement.firstElementChild
-            modal.appendChild(fullItem)
-            modal.style.display = 'block' // show modal
-            fullItem.style.display = 'inline-block' // show modal
-        }
-    }
 
 }, false)
