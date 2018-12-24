dd0786677b1635fda9790c2bdd644fb282f7b3a8
Kees van Ekeren <kg.v.ekeren@gmail.com>
2018-12-24 16:39:49 +0100

    First draft suzannevansoes.com. Fix prettify config typo.

---
+++ suzannevansoest.com/plugins/paintings.py
@@ -0,0 +1,55 @@
+import yaml
+
+ORDER = 999
+PAINTINGS_PATH = 'paintings/'
+
+
+def preBuild(site):
+    """
+    Every painting is defined by a YAML file.
+    This function:
+        - Loads the variables defined by these files
+        - Converts these to a list of dicts representing all paintings
+        - Makes them available for use across the site
+    """
+
+    PAINTINGS = []
+    id_seq = 0
+
+    # Build all the paintings
+    for page in site.pages():
+        if page.path.startswith(PAINTINGS_PATH) and page.path.endswith('.yml'):
+
+            ctx = yaml.load(page.data())
+
+            # Build a context for each post
+            paintingContext = {}
+            paintingContext['id'] = id_seq
+            paintingContext['name'] = ctx['name']
+            paintingContext['width'] = ctx['width']
+            paintingContext['height'] = ctx['height']
+            paintingContext['materials'] = ctx['materials']
+            paintingContext['date_added'] = ctx['date_added']
+            paintingContext['main_image'] = site.get_url_for_static(ctx['main_image'])
+            paintingContext['short_description'] = ctx['short_description']
+
+            PAINTINGS.append(paintingContext)
+            id_seq += 1
+    PAINTINGS = sorted(PAINTINGS, key=lambda x: x['date_added'], reverse=True)
+    setattr(site, 'paintings', PAINTINGS)
+
+
+def preBuildPage(site, page, context, data):
+    """
+    Add the list of posts to every page context so we can
+    access them from wherever on the site.
+
+    Determine how many items we add per column on the homepage
+    """
+    paintings = site.paintings
+    paintings_first_col = [paintings[i] for i in range(len(paintings) // 2)]
+    paintings_second_col = [paintings[i] for i in range(len(paintings) // 2, len(paintings))]
+    context['paintings_first_col'] = paintings_first_col
+    context['paintings_second_col'] = paintings_second_col
+
+    return context, data
